d78f827d62410c705f58d61f80abfd9cb9feb711
Charlie <peabody00@gmail.com>
2021-06-08 08:37:11 -0500

    commit tree 398e1965bf4b11c8e9a9eb4bd4cebfa85faaabf0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(array) {
+    let year;
+    array.find( object => {
+      if (object["result"] === "W") {
+        year = object["year"]
+      }    
+    })
+    return year;
+}
